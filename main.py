9ee2cd6dff1d59e9f4235c3044c0653ffcd56f3a
Saif1041995 <159066619+Saif1041995@users.noreply.github.com>
2025-10-01 12:29:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,6 @@ DATA_DIR = Path("data"); DATA_DIR.mkdir(exist_ok=True)
 
 def safe(n: str) -> str: return os.path.basename(n)
 
-# read the db.xlsx file and return as json
-@app.get("/get_db")
-def get_db():    
-    df = pd.read_csv("data_uploaded.csv")
-    return JSONResponse(df.to_dict(orient="records"))
-
 @app.get("/get_db2")
 def get_db():    
     df = pd.read_csv("db.xlsx")
@@ -50,5 +44,6 @@ def to_json(filename: str, sheet: int | str | None = None, nrows: int | None = N
     if not p.exists(): raise HTTPException(404, "Not found")
     df = pd.read_excel(p, sheet_name=sheet if sheet is not None else 0, nrows=nrows)
     return JSONResponse(df.to_dict(orient="records"))
+
 
 
